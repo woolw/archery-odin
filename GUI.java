d8d8e6dbd769c15a0bcf04461c5b06ab8b03c681
Luna <marvsei@yahoo.de>
2019-11-25 09:18:03 +0100

    Add files via upload

--- GUI.java
+++ GUI.java
@@ -1,7 +1,10 @@
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.border.EmptyBorder;
@@ -11,12 +14,43 @@ public class GUI extends JFrame {
 	private Spiel spiel;
 	
 	private JPanel contentPane;
+	
+	private JLabel pfeil[];
+	private JLabel figur;
+	private JLabel ball[];
+	
+	private Icon pfeilIcon;
+	private Icon figurIcon;
+	private Icon ballIcon;
 
 	/**
 	 * Create the frame.
 	 */
 	public GUI(int width, int height, Spiel spiel) {
 		this.spiel = spiel;
+		
+		pfeilIcon = new ImageIcon("pfeil.png");
+		figurIcon = new ImageIcon("figur.png");
+		ballIcon = new ImageIcon("ball.png");
+		
+		pfeil = new JLabel[10];
+		ball = new JLabel[30];
+		
+		for(int i=0;i<pfeil.length;i++) {
+			pfeil[i] = new JLabel();
+			pfeil[i].setBounds(50, 400+10*i, pfeilIcon.getIconWidth(), pfeilIcon.getIconHeight());
+			contentPane.add(pfeil[i]);
+		}
+		
+		figur = new JLabel();
+		figur.setBounds(100, 120, figurIcon.getIconWidth(), figurIcon.getIconHeight());
+		contentPane.add(figur);
+		
+		for(int i=0;i<ball.length;i++) {
+			ball[i] = new JLabel();
+			ball[i].setBounds(0, 1500, ballIcon.getIconWidth(), ballIcon.getIconHeight());
+			contentPane.add(ball[i]);
+		}
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, width, height);
@@ -51,5 +85,9 @@ public class GUI extends JFrame {
 		
 		this.contentPane.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "schiessen");
 		this.contentPane.getActionMap().put("schiessen", schiessen);
+	}
+	
+	public void aktualisiereBild() {
+		
 	}
 }
